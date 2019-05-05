3965cc6fdb103bad2d46844d7041d3fdb020b4ca
paul newell <38892817+newell-paul@users.noreply.github.com>
2019-05-05 18:33:56 +0100

    Update parallel_steps.rb

--- features/step_definitions/parallel_steps.rb
+++ features/step_definitions/parallel_steps.rb
@@ -6,11 +6,11 @@ end
 Then(/^I can validate the first (\d+) links in the sitemap xml$/) do |max|
   links = @xml_document.css('loc').map(&:text)[1..max]
 
-  Parallel.map(links, in_threads: 10) do |link|
+  Parallel.map_with_index(links, in_threads: 10) do |link, index|
     response = Net::HTTP.get_response(URI(link))
 
     valid_link = response.is_a?(Net::HTTPSuccess)
-    print "#{link}  #{valid_link}\n"
+    puts '#{index} #{link} #{valid_link}'
     expect(valid_link).to be true
   end
 end
